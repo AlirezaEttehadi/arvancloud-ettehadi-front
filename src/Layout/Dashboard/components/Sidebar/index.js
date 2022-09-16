2f3ac3a4e794e636d88465e5aa5c54bb1fa72469
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-16 19:17:25 +0430

    sidebar improvement

--- src/Layout/Dashboard/components/Sidebar/index.js
+++ src/Layout/Dashboard/components/Sidebar/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./index.scss";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar-container vh-100">
+      <div className="text-white p-2">Post</div>
+    </div>
+  );
 }
--- src/Layout/Dashboard/index.js
+++ src/Layout/Dashboard/index.js
@@ -6,7 +6,7 @@ export default function Dashboard({ children }) {
   return (
     <div>
       <Header />
-      <div>
+      <div className="d-flex">
         <Sidebar />
         {children}
       </div>
