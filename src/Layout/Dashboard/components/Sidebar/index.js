baa0153b67308137cd9a5860fa45455065e38ab5
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-16 21:12:55 +0430

    sidebar added

--- src/Layout/Dashboard/components/Sidebar/index.js
+++ src/Layout/Dashboard/components/Sidebar/index.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Sidebar() {
   return (
     <div className="sidebar-container vh-100">
-      <div className="text-white p-2">Post</div>
+      <div class="vertical-menu">
+        <a class="active">All Articles</a>
+        <a>New Article</a>
+      </div>
     </div>
   );
 }
