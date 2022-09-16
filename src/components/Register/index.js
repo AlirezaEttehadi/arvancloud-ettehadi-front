e64f9b359aeed918d089249c80ac838d0e9517a7
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-16 15:48:37 +0430

    ui of register component

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "./index.scss";
 
 export default function Register() {
-  return <div>Register</div>;
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  return (
+    <div className="d-flex align-items-center justify-content-center vw-100 vh-100 position-relative">
+      <div className="register-form p-4 mx-3">
+        <h1 className="register-header text-center mb-4">REGISTER</h1>
+        <div>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicUser">
+              <Form.Label>User</Form.Label>
+              <Form.Control required type="text" placeholder="Enter username" />
+              <Form.Control.Feedback type="invalid">
+                Required field
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control required type="email" placeholder="Enter email" />
+              <Form.Control.Feedback type="invalid">
+                Required field
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control required type="password" placeholder="Password" />
+              <Form.Control.Feedback type="invalid">
+                Required field
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button className="mt-4" variant="primary" type="submit" block>
+              Register
+            </Button>
+          </Form>
+        </div>
+        <div className="mt-3">
+          <span className="mr-2">Already Registered?</span>
+          <span className="font-weight-bold">Login</span>
+        </div>
+      </div>
+    </div>
+  );
 }
