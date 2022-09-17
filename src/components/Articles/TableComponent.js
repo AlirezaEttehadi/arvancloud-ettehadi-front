350c124d32c199b9224028b13c8e7c9b47dd1e64
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-17 13:32:53 +0430

    add table component

---
+++ src/components/Articles/TableComponent.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+
+export default function TableComponent() {
+  return (
+    <Table hover responsive>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Tags</th>
+          <th>Exerpt</th>
+          <th>Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Larry the Bird</td>
+          <td>Larry the Bird</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import TableComponent from "./TableComponent";
 
 export default function Articles() {
-  return <div>Articles</div>;
+  return (
+    <div className="p-3 w-100">
+      <h1 className="mb-4">All Posts</h1>
+      <TableComponent />
+    </div>
+  );
 }
