b963d655bbec3165df0340336514b8d8ad2a8423
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-18 01:29:21 +0430

    add delete modal components

---
+++ src/components/Articles/DeleteModal.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+
+export default function DeleteModal(props) {
+  return (
+    <Modal {...props} size="md" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Delete Article
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <span>Are you sure to delete article?</span>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-dark" onClick={props.onHide}>
+          No
+        </Button>
+        <Button variant="danger" onClick={props.onHide}>
+          Yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
