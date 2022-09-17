2cd03023aef702840a93fd95a1e3877445b47005
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-17 12:49:17 +0430

    add hamburger menu

--- src/Layout/Dashboard/components/Header/index.js
+++ src/Layout/Dashboard/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Navbar } from "react-bootstrap";
+import HamburgerMenu from "./HamburgerMenu";
 
 export default function Header() {
   return (
@@ -9,6 +10,7 @@ export default function Header() {
       expand="lg"
       className="d-flex align-items-center justify-content-between"
     >
+      <HamburgerMenu className="d-md-none d-inline-block" />
       <div>
         <Navbar.Brand className="mr-3">Arvan Challenge</Navbar.Brand>
         <span className="text-white">Welcome alireza</span>
