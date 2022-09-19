57ed432fc4cea88f09325cb9ebdeee03c0676c83
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-20 01:28:56 +0430

    add sidebar navigation logic

--- src/Layout/Dashboard/components/Sidebar/index.js
+++ src/Layout/Dashboard/components/Sidebar/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className="sidebar-container">
       <div className="vertical-menu">
-        <a className="active">All Articles</a>
-        <a>New Article</a>
+        <NavLink
+          end
+          to="/articles/"
+          style={({ isActive }) => ({
+            backgroundColor: isActive && "rgba(255, 255, 255, 0.15)",
+          })}
+        >
+          All Articles
+        </NavLink>
+        <NavLink
+          to="/articles/create"
+          style={({ isActive }) => ({
+            backgroundColor: isActive && "rgba(255, 255, 255, 0.15)",
+          })}
+        >
+          New Article
+        </NavLink>
       </div>
     </div>
   );
