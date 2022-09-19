0da1546479c21af8eebdabb57eb46f96aea7eb8c
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-19 14:21:36 +0430

    make services mock structure

--- src/services/articles/index.js
+++ src/services/articles/index.js
@@ -1,5 +1,35 @@
 import axios from "axios";
 
-export const getArticlesRequest = () => {
-  return axios.get("https://api.realworld.io/api/articles");
+export const getArticlesRequest = (page = 1, pageSize = 20) => {
+  return axios.get(
+    `https://api.realworld.io/api/articles?limit=${pageSize}&offset=${
+      page * pageSize
+    }`
+  );
+};
+export const createArticleRequest = (slug) => {
+  return axios.get("https://api.realworld.io/api/articles/", {
+    body: "sdfsd",
+    description: "adsfsdf",
+    title: "SDFsfd",
+    tags: ["Sdfsdf", "sdfsdf"],
+  });
+};
+export const getArticleRequest = (slug) => {
+  return axios.get(`https://api.realworld.io/api/articles/${slug}`);
+};
+export const editArticleRequest = (slug) => {
+  return axios.put(`https://api.realworld.io/api/articles/${slug}`, {
+    author: "sdf",
+    body: "sdfsdf",
+    createdAt: "sdf",
+    id: "sdfsf",
+    updatedAt: "Sdfsdf",
+  });
+};
+export const deleteArticleRequest = (slug) => {
+  return axios.delete(`https://api.realworld.io/api/articles/${slug}`);
+};
+export const getTagsRequest = () => {
+  return axios.delete("https://api.realworld.io/api/tags");
 };
