84b74fdab86c97e436fc22089a53c11d48359592
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-20 02:12:57 +0430

    add spinner for table

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -4,6 +4,7 @@ import TablePagination from "./TablePagination";
 import DeleteModal from "./DeleteModal";
 import { connect } from "react-redux";
 import { deleteArticle, getArticles } from "../../redux/articles/action";
+import Spinner from "react-bootstrap/Spinner";
 
 function Articles({ _getArticles, articles, _deleteArticle, loading }) {
   const [deleleModalState, setDeleteModalState] = useState({
@@ -17,17 +18,23 @@ function Articles({ _getArticles, articles, _deleteArticle, loading }) {
   return (
     <div className="d-flex flex-column pt-3 px-3 w-100 vh-100">
       <h1 className="mb-4">All Posts</h1>
-      <TableComponent
-        articles={articles}
-        setDeleteModalState={setDeleteModalState}
-      />
-      <TablePagination />
-      <DeleteModal
-        show={deleleModalState.isOpen}
-        slug={deleleModalState.slug}
-        onHide={() => setDeleteModalState({ isOpen: false, slug: null })}
-        _deleteArticle={_deleteArticle}
-      />
+      {loading ? (
+        <Spinner animation="grow" size="lg" />
+      ) : (
+        <>
+          <TableComponent
+            articles={articles}
+            setDeleteModalState={setDeleteModalState}
+          />
+          <TablePagination />
+          <DeleteModal
+            show={deleleModalState.isOpen}
+            slug={deleleModalState.slug}
+            onHide={() => setDeleteModalState({ isOpen: false, slug: null })}
+            _deleteArticle={_deleteArticle}
+          />
+        </>
+      )}
     </div>
   );
 }
