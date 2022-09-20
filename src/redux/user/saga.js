d8138ef8f60c69bd73822014933b3b192a2d6081
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-20 21:21:08 +0430

    bug fix for set user dispatch

--- src/redux/user/saga.js
+++ src/redux/user/saga.js
@@ -21,7 +21,7 @@ function* registerSaga(action) {
     yield put(startLoading());
     const user = yield call(request, "post", usersEntity, action.payload);
     localStorage.setItem("user", JSON.stringify(user.data.user));
-    yield put(setUser(user.data.data));
+    yield put(setUser(user.data.user));
     yield put(stopLoading());
   } catch (e) {
     yield put(stopLoading());
@@ -33,7 +33,7 @@ function* loginSaga(action) {
     yield put(startLoading());
     const user = yield call(request, "post", loginUserEntity, action.payload);
     localStorage.setItem("user", JSON.stringify(user.data.user));
-    yield put(setUser(user.data.data));
+    yield put(setUser(user.data.user));
     yield put(stopLoading());
   } catch (e) {
     yield put(stopLoading());
