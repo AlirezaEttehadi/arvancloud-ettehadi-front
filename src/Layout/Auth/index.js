3ffeb97f89fdc9e8b1b3cc5b545ed60b2076bd41
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-21 11:29:38 +0430

    delete extra props from snackbar message component

--- src/Layout/Auth/index.js
+++ src/Layout/Auth/index.js
@@ -4,12 +4,7 @@ import SnackbarMessage from "../components/SnackbarMessage";
 export default function Auth({ children }) {
   return (
     <div className="d-flex align-items-center justify-content-center vw-100 vh-100 position-relative">
-      <SnackbarMessage
-        variant="danger"
-        shortMessage="Login Failed!"
-        longMessage="User name and/or Password is invalid"
-        layoutType="auth"
-      />
+      <SnackbarMessage />
       {children}
     </div>
   );
