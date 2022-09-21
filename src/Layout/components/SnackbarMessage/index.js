541af4479d2a2511c9f5cddac8a3439d0e91d9a1
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-21 12:09:28 +0430

    add null checker

--- src/Layout/components/SnackbarMessage/index.js
+++ src/Layout/components/SnackbarMessage/index.js
@@ -5,20 +5,20 @@ import { connect } from "react-redux";
 function SnackbarMessage({ snackbarMessage }) {
   const [show, setShow] = useState(false);
   useEffect(() => {
-    setShow(snackbarMessage.show);
+    setShow(snackbarMessage?.show);
   }, [snackbarMessage]);
   if (show) {
     return (
       <Alert
-        className={`position-absolute m-3 ${snackbarMessage.layoutType}Snackbar`}
-        variant={snackbarMessage.variant}
+        className={`position-absolute m-3 ${snackbarMessage?.layoutType}Snackbar`}
+        variant={snackbarMessage?.variant}
         dismissible
         onClose={() => setShow(false)}
       >
         <span className="font-weight-bold mr-1">
-          {snackbarMessage.shortMessage}
+          {snackbarMessage?.shortMessage}
         </span>
-        <span>{snackbarMessage.longMessage}</span>
+        <span>{snackbarMessage?.longMessage}</span>
       </Alert>
     );
   } else {
